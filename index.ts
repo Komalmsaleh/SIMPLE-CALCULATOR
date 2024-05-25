411782b0eca9489e9d4f674e3277720bee477fe6
Komalmsaleh <160410944+Komalmsaleh@users.noreply.github.com>
2024-05-25 20:24:43 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter Your FirstNumber", type: "number", name: "FirstNumber" },
+  {
+    message: "Enter Your SecondNumber",
+    type: "Number",
+    name: "SecondNumber",
+  },
+  {
+    message: "Select One Oprator To Perfom Action",
+    type: "list",
+    name: "Oprator",
+    choices: ["ADDITION", "SUBSTRACTION", "MULTIPLICATION", "DIVISION"],
+  },
+]);
+
+  if (answer.Oprator === "ADDITION"){
+    console.log(answer.FirstNumber + answer. SecondNumber);
+  } else if (answer.Oprator === "SUBSTRACTION"){
+    console.log(answer.FirstNumber - answer. SecondNumber);
+  }  else if (answer.Oprator === "MULTIPICATION"){
+    console.log(answer.FirstNumber * answer. SecondNumber);
+  }   else if (answer.Oprator === "DIVISION"){
+    console.log(answer.FirstNumber / answer. SecondNumber);
+  } 
+
+  else{"Please Select One Oprator"}
+
